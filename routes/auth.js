c7c2759124bbc8ba9487fe743f406251915145c6
KhanhLinh <linh105463@donga.edu.vn>
2025-05-05 02:41:17 +0700

    hh

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const db = require('../db');
+const db = require('../config/db');
 const router = express.Router();
 
 const JWT_SECRET = process.env.JWT_SECRET;
